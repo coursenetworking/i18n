e9b3d364740e21bc77220068053651c215cfec4e
King Chung <kingchung@outlook.com>
2015-12-18 13:33:39 +0800

    Update config file.

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify');
 
 gulp.task('default', function(){
-    gulp.src('src/app.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/'));
+    // gulp.src('src/js/app.js')
+    //     .pipe(uglify())
+    //     .pipe(gulp.dest('dist/js/'));
 });
